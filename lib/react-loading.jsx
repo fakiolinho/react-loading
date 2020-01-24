f79705bf42607c4d4524df56f1436926fcbd555b
IdealSystemsMCP <35523248+IdealSystemsMCP@users.noreply.github.com>
2020-01-24 13:26:00 +0100

    Add custom styles

--- lib/react-loading.jsx
+++ lib/react-loading.jsx
@@ -53,19 +53,19 @@ export default class Loading extends Component {
 
   render() {
     const {
-      color, delay, type, height, width, ...restProps
+      color, delay, type, height, width, style, ...restProps
     } = this.props;
     const selectedType = this.state.delayed ? 'blank' : type;
     const svg = svgSources[selectedType];
-    const style = {
-      fill: color,
-      height,
-      width,
-    };
 
     return (
       <div
-        style={style}
+        style={{
+          fill: color,
+          height,
+          width,
+          ...style,
+        }}
         dangerouslySetInnerHTML={{ __html: svg }}
         {...restProps}
       />
